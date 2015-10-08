3984812d88132105549533348332b5b512cf29fd
Ninhydrin <the.nocturne.of.daybreak@gmail.com>
2015-10-09 00:04:10 +0900

    adjust helm

--- inits/07_helm.el
+++ inits/07_helm.el
@@ -13,3 +13,9 @@
 
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key global-map (kbd "M-x")     'helm-M-x)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-r") 'helm-recentf)
+(define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+(define-key global-map (kbd "C-c i")   'helm-imenu)
+(define-key global-map (kbd "C-x b")   'helm-buffers-list)
