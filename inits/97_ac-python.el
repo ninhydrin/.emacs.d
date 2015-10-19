84d2f3362d765d234cd9993c5049d13dc19e7ec2
Ninhydrin <the.nocturne.of.daybreak@gmail.com>
2015-10-19 10:54:30 +0900

    adjust ac-python

--- inits/97_ac-python.el
+++ inits/97_ac-python.el
@@ -1,6 +1,6 @@
 ;;; 97_ac-python.el ---                              -*- lexical-binding: t; -*-
-(require 'ac-python)
-(add-to-list 'ac-modes 'python-2-mode)
+;(require 'ac-python)
+;(add-to-list 'ac-modes 'python-2-mode)
 ;; Copyright (C) 2015  横島府
 
 ;; Author: 横島府 <yokoshimakura@yokoshimafu-no-MacBook-Air.local>
